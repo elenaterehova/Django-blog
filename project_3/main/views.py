a18491c350de088e037dec9e6908a4ad138a2843
Пользователь <tterekhovaa@mail.ru>
2023-08-12 22:02:53 +0300

    добавить посты, пагинатор, страницу поста

--- project_3/main/views.py
+++ project_3/main/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from django.core.paginator import Paginator
+from .models import Post
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'main/home.html')
+        posts = Post.objects.all()
+        paginator = Paginator(posts, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'main/home.html', {'page_obj': page_obj})
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'main/post_detail.html', context={'post': post})
