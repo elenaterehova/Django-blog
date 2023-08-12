ce9b1331b8b707a4bafbd6db3b75f602558fa073
Пользователь <tterekhovaa@mail.ru>
2023-08-13 01:54:39 +0300

    добавить регистрацию и авторизацию

--- project_3/main/urls.py
+++ project_3/main/urls.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import MainView, PostDetailView
+from .views import MainView, PostDetailView, SignUpView, SignInView
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
-    path('main/<slug>/', PostDetailView.as_view(), name='post_detail')
+    path('main/<slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signin/', SignInView.as_view(), name='signin'),
+    path('signout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='signout'),
+
 ]
--- project_3/main/views.py
+++ project_3/main/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post
+from .forms import SignUpForm, SignInForm
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponseRedirect
+
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
@@ -13,7 +17,39 @@ class MainView(View):
 
         return render(request, 'main/home.html', {'page_obj': page_obj})
 
+
 class PostDetailView(View):
     def get(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, url=slug)
         return render(request, 'main/post_detail.html', context={'post': post})
+
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignUpForm()
+        return render(request, 'main/signup.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'signup.html', context={'form': form})
+
+class SignInView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignInForm()
+        return render(request, 'main/signin.html', context={'form': form})
+    def post(self, request, *args, **kwargs):
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'main/signin.html', context={'form': form})
+
